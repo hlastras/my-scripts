7575b304ad526f6f0ede4cb5212dd841d169ef7a
Hector Arranz <hector_lastras@hotmail.com>
2018-11-18 16:40:25 +0100

    Arreglada instalacion Chrome

--- instalacion-linux.sh
+++ instalacion-linux.sh
@@ -19,7 +19,7 @@ sudo apt-get update
 
 #### Google Chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb
 
 #### qBittorrent
